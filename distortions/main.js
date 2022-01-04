ea04b0c43b8820794249f0ba4852224a4e5441c2
danyalutsevich <luchevich31@gmail.com>
2022-01-04 22:02:51 +0200

    time uniform

--- distortions/main.js
+++ distortions/main.js
@@ -1,40 +1,50 @@
 import * as THREE from 'three'
-
+import * as dat from 'dat.gui'
 import vert from './shaders/vertex.glsl'
 import frag from './shaders/fragment.glsl'
 
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 const scene = new THREE.Scene()
-const renderer = new THREE.WebGL1Renderer({ antialias: true })
+const renderer = new THREE.WebGL1Renderer(/*{ antialias: true }*/)
 
 renderer.setSize(innerWidth, innerHeight)
-renderer.setPixelRatio(window.devicePixelRatio)
+// renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 camera.position.set(0, 0, 2)
 
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(2.5, 2.5), new THREE.ShaderMaterial({
+let uniforms = {
+
+  time: { type: 'f', value: 0. },
+  photo: { value: new THREE.TextureLoader().load('./yeezus.jpg') },
+  displacement:{value: new THREE.TextureLoader().load('./disp.jpg')},
+
+}
+
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(2.5, 2.5, 10, 10), new THREE.ShaderMaterial({
 
   vertexShader: vert,
   fragmentShader: frag,
-  uniforms: {
-    
-    photo: {
-     value: new THREE.TextureLoader().load('./yeezus.jpg')
-    }
-  }
+  uniforms
+  
 }))
 
 scene.add(plane)
 
+let milliseconds=0;
 
 
 function animate() {
+  
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-
-
-
+  
+  // const clock = new Date().getSeconds();
+  milliseconds+=new Date().getMilliseconds();
+  uniforms.time = {type:"f",value:milliseconds}
+  
+  // console.log(new Date().getMilliseconds())
 }
 
 animate()
--- distortions/shaders/fragment.glsl
+++ distortions/shaders/fragment.glsl
@@ -1,10 +1,24 @@
 
+uniform float time;
 uniform sampler2D photo;
+uniform sampler2D displacement;
 varying vec2 vUv;
 varying vec3 vertexNormal;
 
 void main(){
     
-    gl_FragColor=vec4(texture2D(photo,vUv).xyz,1.);
+    vec3 disp = texture2D(displacement,vUv).xyz;
+
+    vec2 dispvUv = vec2(vUv.x +0.01*sin(vUv.y*+time/30.),vUv.y);
+    // vec2 dispvUv = vec2(vUv.x +disp.x,vUv.y);
+
+    vec3 ph = texture2D(photo,dispvUv).xyz;
+    
+
+
+
+
+    gl_FragColor=vec4(ph,1.);
+    // gl_FragColor=vec4(time/10.);
     
 }
